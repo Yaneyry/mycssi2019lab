9d27927b0e9792896940383ae806ed5dc65f0e81
Yaneyry DelfinMartinez <delfi010@csusm.edu>
2019-07-24 12:04:49 -0700

    message 1

---
+++ command-line/WE LEARN/M2-javaScript/L1-JSIntro/script.js
@@ -0,0 +1,15 @@
+console.log("script running!");
+
+let name = 'charlie';
+console.log('Hello' + name + '!');
+let age = 15;
+console.log('You are' + age + 'years old');
+
+
+
+if(age >=16){
+console.log("I can drive!")
+}
+else
+{console.log("I cant drive!")
+}
---
+++ command-line/WE LEARN/M2-javaScript/L2-JSFunction/Console/script.js
@@ -0,0 +1,23 @@
+const greetPeople = (name1, name2) => {
+  console.log('Hello, ' + name1 + ' and ' + name2 + '!');
+}
+
+const getFullName = (firstName, lastName) => {
+  return firstName + ' ' + lastName;
+}
+
+
+let radius = 10;
+let area = Math.PI * radius * radius;
+radius = radius * 2;
+Area=
+Math.PI * Math.pow(radius, 2)
+let Width = Math.round(Math.random() * 100);
+let Height = math.round(Math.random() * 50);
+let Circumference = width * 2 + height * 2;
+
+let n = 1;
+setInterval(() => {
+  n = n + 1;
+  console.log(n);
+}, 1000);
