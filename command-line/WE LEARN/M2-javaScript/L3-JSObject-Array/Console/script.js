10ba0196aefab0e25b195de4af8ff1e77a6a12f7
Yaneyry DelfinMartinez <delfi010@csusm.edu>
2019-07-25 12:08:09 -0700

    message 1

---
+++ command-line/WE LEARN/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,30 @@
+const article = {
+  name : 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Joe Corgi',
+  title: 'Senior Canine Editor'
+}
+};
+
+
+ const favfood = {
+   food1: 'Enchiladas',
+   food2: 'Mariscos',
+   food3: 'chicken nuggets',
+   food4: 'chips',
+   food5:  'ice cream'
+ };
+
+ console.log(favfood)
+
+
+const seasons = ['spring', 'summer', 'fall', 'winter'];
+const friends = ['Alice', 'Bob', 'Charlie'];
+const numbers = [5, 10, 50, 16];
+
+
+
+const classes = [];
+classes.push([course: 'CS 111'])
---
+++ command-line/WE LEARN/M2-javaScript/L4-JSLoop-DOM/loop/script.js
@@ -0,0 +1,16 @@
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+const findTotal = ((numbers) => {let sum = 0;
+  for(let i = 0; i<numbers.length; i++) {
+    sum = sum+numbers[i];
+  }});
+
+const buttons = document.querySelectorAll ('#box');
+const box = document.querySelector ('#box');
+buttons.forEach((button) => {
+  button.addEventListener('click', () =>{
+    const color =
+  })
+})
