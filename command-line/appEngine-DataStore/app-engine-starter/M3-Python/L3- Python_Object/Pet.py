621f7dfe3d492e674b6384c199f292cc5c421c37
Yaneyry DelfinMartinez <delfi010@csusm.edu>
2019-08-01 10:18:05 -0700

    message 1

---
+++ command-line/appEngine-DataStore/app-engine-starter/M3-Python/L3- Python_Object/Pet.py
@@ -0,0 +1,46 @@
+pet = {
+"name" : "Doggo",
+"animal": "dog",
+"species": "labrador",
+"age": 5
+}
+
+class Pet(object):
+def__init__ (self, name, age):
+self.name = name
+slef.age = age
+
+my_pet = Pet("Lulu", 4)
+    print("My pet % is % years old" % (my_pet.name, my_pet.age))
+
+class Pet(object):
+def__init__()
+
+Class Pet(object):
+def__init__(self, name, age, animal):
+self.name = name
+self.age = age
+self.animal = animal
+self.is_hungry = False
+self.mood = "happy"
+
+#the pet is eating
+def eat(self):
+    print("> % is eating..." % self.name)
+    if self.hungry:
+    self.is_hungry = #false
+else: #if the first test was false and the pet wasn't hungry
+    print("> %s may have eaten too much." % self.name)
+    self.mood = "lethargic"
+
+my_pet = Pet("Lulu", 4, "dog")
+
+my_pet.is_hungry = # True manually set to true
+print("Is my pet hungry? %s" % my_pet.is_hungry)
+my_pet.eat() # calls the eat method is defined above
+print("How about now? %s" % my_pet.is_hungry)
+print("My pet is feeling %s" % my_pet.mood) s
+
+def move(self)
+print("> % is sleeping." % self.name)
+if self.
